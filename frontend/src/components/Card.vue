f91985a1167f17eb5ada15d3198ac1e8aad89e77
Voldi Zola <voldi.zola@wister.fr>
2022-10-07 12:07:26 +0200

    Create vue js with card

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,30 +1,32 @@
 <script>
-export default {name : 'Card' }
+import '@coreui/coreui/dist/css/coreui.min.css'
+import { CCard, CCardImage, CCardTitle, CCardText, CButton, CCardBody   } from '@coreui/vue';
+export default {name : 'Card',
+                components : {CCard, CCardImage, CCardTitle, CCardText, CButton, CCardBody
+                }}
 </script>
 
 <template>
-  <h1>Card voici</h1>
-  
-  <img src="src/assets/naruto.jpg">
-  <p>As Naruto was born, his home village of Konoha came under
-    attack by a nine-tailed fox demon which was sealed in him at
-    infancy to protect the people of the village. Due to this fact
-    he became distrusted and resented by the people around him during his childhood.
-    He, however, aspires to become the Hokage; a title given to the leader
-    of Hidden Leaf Village, and as the most poorly performing student in his ninja school,
-    he strives to prove his worth to his peers and fellow villagers.
-    He is known for his often-used catchphrase "Dattebayo",
-    and is connected to most other characters around him
-    by either friendship or rivalry.</p>
+  <CCard style="width: 30rem" >
+    <CCardBody>
+      <CCardImage orientation="top" src="src/assets/naruto.jpg"/>
+      <CCardText>As Naruto was born, his home village of Konoha came under
+        attack by a nine-tailed fox demon which was sealed in him at
+        infancy to protect the people of the village. Due to this fact
+        he became distrusted and resented by the people around him during his childhood.
+        He, however, aspires to become the Hokage; a title given to the leader
+      </CCardText>
 
-  <p>
-    <label for="name">Name</label>
-    <input
-        id="name"
-        v-model="name"
-        type="text"
-        name="name"
-    >
-  </p>
-  <button>Button</button>
+      <p>
+        <label for="name">Name</label>
+        <input
+            id="name"
+            v-model="name"
+            type="text"
+            name="name"
+        >
+      </p>
+      <CButton color="primary">Valider</CButton>
+    </CCardBody>
+  </CCard>
 </template>
