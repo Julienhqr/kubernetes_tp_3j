0aa8d0070d23e36a400b5bd7321145d249dc2473
Voldi Zola <voldi.zola@wister.fr>
2022-10-10 16:23:31 +0200

    update src images

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,6 +1,7 @@
 <script>
 import '@coreui/coreui/dist/css/coreui.min.css'
 import { CCard, CCardImage, CCardTitle, CCardText, CButton, CCardBody   } from '@coreui/vue';
+
 export default {name : 'Card',
                 components : {CCard, CCardImage, CCardTitle, CCardText, CButton, CCardBody
                 }}
@@ -9,7 +10,7 @@ export default {name : 'Card',
 <template>
   <CCard style="width: 30rem" >
     <CCardBody>
-      <CCardImage orientation="top" src="src/assets/naruto.jpg"/>
+      <CCardImage orientation="top" src="https://picsum.photos/1024/480/?image=54"></CCardImage>
       <CCardText>As Naruto was born, his home village of Konoha came under
         attack by a nine-tailed fox demon which was sealed in him at
         infancy to protect the people of the village. Due to this fact
@@ -18,15 +19,16 @@ export default {name : 'Card',
       </CCardText>
 
       <p>
-        <label for="name">Name</label>
         <input
             id="name"
             v-model="name"
             type="text"
             name="name"
+            placeholder="name"
         >
       </p>
       <CButton color="primary">Valider</CButton>
     </CCardBody>
   </CCard>
 </template>
+
