4d662463dd6a72bb17bee568ec3fd61401be8ed6
RAICOVITCH Lucas (ITNOVEM) <lucasraico@gmail.com>
2022-10-11 16:05:00 +0200

    feat: 🎸 try fix

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,16 +1,19 @@
 <script>
 import '@coreui/coreui/dist/css/coreui.min.css'
-import { CCard, CCardImage, CCardTitle, CCardText, CButton, CCardBody   } from '@coreui/vue';
+import { CCard, CCardImage, CCardTitle, CCardText, CButton, CCardBody } from '@coreui/vue';
 
-export default {name : 'Card',
-                components : {CCard, CCardImage, CCardTitle, CCardText, CButton, CCardBody
-                }}
+export default {
+  name: 'Card',
+  components: {
+    CCard, CCardImage, CCardTitle, CCardText, CButton, CCardBody
+  }
+}
 </script>
 
 <template>
-  <CCard style="width: 30rem" >
+  <CCard style="width: 30rem">
     <CCardBody>
-      <CCardImage orientation="top" src="/img/naruto.jpg"></CCardImage>
+      <CCardImage orientation="top" src="https://thispersondoesnotexist.com/image"></CCardImage>
       <CCardText>As Naruto was born, his home village of Konoha came under
         attack by a nine-tailed fox demon which was sealed in him at
         infancy to protect the people of the village. Due to this fact
@@ -19,13 +22,7 @@ export default {name : 'Card',
       </CCardText>
 
       <p>
-        <input
-            id="name"
-            v-model="name"
-            type="text"
-            name="name"
-            placeholder="name"
-        >
+        <input id="name" v-model="name" type="text" name="name" placeholder="name">
       </p>
       <CButton color="primary">Valider</CButton>
     </CCardBody>
